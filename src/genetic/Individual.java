07141df1a4968b5ef7ffa3bff629c54f255eb828
Salim Malakouti <salim.malakouti@gmail.com>
2012-12-09 01:43:21 +0330

    Initial Submit

---
+++ src/genetic/Individual.java
@@ -0,0 +1,40 @@
+package genetic;
+public class Individual {
+	private int[] sequence;
+	private double value;
+	
+	public Individual(int[] seq) {
+		sequence = seq;
+	}
+
+
+	public int[] getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(int[] sequence) {
+		this.sequence = sequence;
+	}
+
+
+	public double getValue() {
+		return value;
+	}
+
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		String tmp="[ ";
+		for (int i = 0; i < sequence.length; i++) {
+			if(i!=0)
+				tmp+=" , ";
+			tmp+=sequence[i];
+		}
+		tmp+=" ]";
+		return tmp+"\n   Size: "+sequence.length+"\n   Value: "+value;
+	}
+	
+}
